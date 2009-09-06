3fef584f2cfd07b476c7f2a55985f14f4427bb9e
Craig Condit <ccondit@randomcoder.org>
2009-09-06 04:23:07 +0000

    ObjectMessage implementation.
    git-svn-id: https://randomcoder.org/repos/public/tinyjms/trunk@960 5bee6cb3-3d18-0410-8c93-a642edd49b48

--- src/main/java/com/randomcoder/tinyjms/client/TinyJmsObjectMessage.java
+++ src/main/java/com/randomcoder/tinyjms/client/TinyJmsObjectMessage.java
@@ -4,21 +4,53 @@ import java.io.Serializable;
 
 import javax.jms.*;
 
+/**
+ * TinyJms implementation of {@link ObjectMessage}.
+ */
 public class TinyJmsObjectMessage extends TinyJmsMessage implements ObjectMessage
 {
+	private Serializable object;
 
+	/**
+	 * Gets the serializable object containing this message's data. The default
+	 * value is <code>null</code>.
+	 * 
+	 * @return the serializable object containing this message's data
+	 * @throws JMSException
+	 *           if the JMS provider fails to get the object due to some internal
+	 *           error.
+	 * @throws MessageFormatException
+	 *           if object deserialization fails.
+	 */
 	@Override
 	public Serializable getObject() throws JMSException
 	{
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException();
+		// TODO deserialize
+		return object;
 	}
 
+	/**
+	 * Sets the serializable object containing this message's data. It is
+	 * important to note that an <code>ObjectMessage</code> contains a snapshot of
+	 * the object at the time <code>setObject()</code> is called; subsequent
+	 * modifications of the object will have no effect on the
+	 * <code>ObjectMessage</code> body.
+	 * 
+	 * @param object
+	 *          the message's data
+	 * @throws JMSException
+	 *           if the JMS provider fails to set the object due to some internal
+	 *           error.
+	 * @throws MessageFormatException
+	 *           if object serialization fails.
+	 * @throws MessageNotWriteableException
+	 *           if the message is in read-only mode.
+	 */
 	@Override
 	public void setObject(Serializable object) throws JMSException
 	{
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException();		
+		// TODO serialize and verify that message is writable
+		this.object = object;
 	}
 
 }
